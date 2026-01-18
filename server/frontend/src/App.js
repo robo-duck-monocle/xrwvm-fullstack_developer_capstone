8628d5cb53a94d49d4c3c585cc6cbf90ac07a279
robo-duck-monocle <kp3rd.007@gmail.com>
2026-01-17 21:17:26 -0500

    Paused at filling in the dealer reviews by state.

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,13 +1,17 @@
+import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer"
 import LoginPanel from "./components/Login/Login"
 import RegisterPanel from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/login" element={<LoginPanel />} />
-      <Route path="/register" element={<RegisterPanel />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/login" element={<LoginPanel />} />
+            <Route path="/register" element={<RegisterPanel />} />
+            <Route path="/dealers" element={<Dealers />} />
+            <Route path="/dealer/:id" element={<Dealer/>} />
+        </Routes>
+    );
 }
 export default App;
